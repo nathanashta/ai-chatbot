b6748d89fa6550ebd99f6738ba469cb30ca6c3c4
Nathan Ashta <nathanashta@gmail.com>
2019-12-14 16:28:45 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+
+app.use(express.static(__dirname + "/views")); // html
+app.use(express.static(__dirname + "/public")); // js, css, images
+
+const server = app.listen(5000);
+
+const io = require("socket.io")(server);
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+const apiai = require("apiai")(APIAI_SESSION_ID);
+
+io.on("connection", function(socket) {
+  socket.on("chat message", text => {
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: APIAI_SESSION_ID
+    });
+
+    apiaiReq.on("response", response => {
+      let aiText = response.result.fulfillment.speech;
+      socket.emit("bot reply", aiText);
+    });
+
+    apiaiReq.on("error", error => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+  });
+});
