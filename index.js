5c83cd1d9c165829f0ba805ff9cf432e4858b41d
Nathan Ashta <nathanashta@gmail.com>
2019-12-18 14:46:34 -0600

    added comments

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
+// Instantiate express
 const express = require("express");
 const app = express();
+
+// Import .env
 require("dotenv").config();
 
+// Instantiate APIAI (Dialogflow)
 const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+const apiai = require("apiai")(APIAI_SESSION_ID);
 
+// Import HTML, JS, CSS, images
 app.use(express.static(__dirname + "/views")); // html
 app.use(express.static(__dirname + "/public")); // js, css, images
 
+// Instantiate server using socket.io
 const port = process.env.PORT || 5000;
-
 const server = app.listen(port);
-
 const io = require("socket.io")(server);
 
+// Start the server
+console.log("Server is listening on port", port, "...");
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
-const apiai = require("apiai")(APIAI_SESSION_ID);
-
+// On connection to script
 io.on("connection", function(socket) {
+  // On chat message
   socket.on("chat message", text => {
+    // Send API call to APIAI
     let apiaiReq = apiai.textRequest(text, {
       sessionId: APIAI_SESSION_ID
     });
 
+    // Get API response
     apiaiReq.on("response", response => {
       let aiText = response.result.fulfillment.speech;
       socket.emit("bot reply", aiText);
     });
 
+    // Log any error
     apiaiReq.on("error", error => {
       console.log(error);
     });
 
+    // End the request
     apiaiReq.end();
   });
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,25 @@
+// Instantiate speech recognition
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-
-const outputYou = document.querySelector(".output-you");
-const outputBot = document.querySelector(".output-bot");
-
-const socket = io();
-
 recognition.lang = "en-US";
 recognition.interimResults = false;
 
+// Instantiate socket
+const socket = io();
+
+// Get elements of user output and bot output
+const outputYou = document.querySelector(".output-you");
+const outputBot = document.querySelector(".output-bot");
+
+// Add event listener on the button
 document.querySelector("button").addEventListener("click", () => {
   recognition.start();
 });
 
+// On end of recognition
 recognition.addEventListener("result", e => {
+  // Get text and emit the message
   let last = e.results.length - 1;
   let text = e.results[last][0].transcript;
 
@@ -23,6 +28,7 @@ recognition.addEventListener("result", e => {
   socket.emit("chat message", text);
 });
 
+// Function to do text to speech
 function synthVoice(text) {
   const synth = window.speechSynthesis;
   const utterance = new SpeechSynthesisUtterance();
@@ -30,6 +36,7 @@ function synthVoice(text) {
   synth.speak(utterance);
 }
 
+// Socket for bot reply
 socket.on("bot reply", function(replyText) {
   synthVoice(replyText);
 
